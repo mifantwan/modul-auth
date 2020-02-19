f570782047a807d486c6a2eb7fff3e3488a0d8bd
mifantwan <me@mifan.io>
2020-02-19 14:49:59 +0700

    fixing depedency

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { NotificationModule } from './notification/notification.module';
+import { RouterModule, Routes } from 'nest-router';
+
+const routes: Routes = [
+  {
+    path: '/auth',
+    module: AuthModule
+  },
+  {
+    path: '/notification',
+    module: NotificationModule
+  }
+]
 
 @Module({
-  imports: [AuthModule, NotificationModule],
+  imports: [
+    RouterModule.forRoutes(routes),
+    AuthModule,
+    NotificationModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

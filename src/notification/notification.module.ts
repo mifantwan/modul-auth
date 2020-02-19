b8a91bcafe3f274179058a7f8ecec8be21cee12d
mifantwan <me@mifan.io>
2020-02-19 18:15:21 +0700

    add module

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -4,10 +4,31 @@ import { NotificationService } from './notification.service';
 import { EmailModule } from './email/email.module';
 import { WhatsappModule } from './whatsapp/whatsapp.module';
 import { MessageModule } from './message/message.module';
+import { RouterModule, Routes } from 'nest-router';
+
+const routes: Routes = [
+  {
+    path: '/email',
+    module: EmailModule
+  },
+  {
+    path: '/message',
+    module: MessageModule
+  },
+  {
+    path: '/whatsapp',
+    module: WhatsappModule
+  }
+]
 
 @Module({
   controllers: [NotificationController],
   providers: [NotificationService],
-  imports: [EmailModule, WhatsappModule, MessageModule]
+  imports: [
+    RouterModule.forRoutes(routes),
+    EmailModule,
+    WhatsappModule,
+    MessageModule
+  ]
 })
 export class NotificationModule {}
